7b0d4098e79c5c49a4fcab9bec03d479da4625e2
dquyhn-cpu <d.quyhn@gmail.com>
2026-09-23 21:14:22 +0700

    Fix deletion helper syntax

--- facebook_delete_post.py
+++ facebook_delete_post.py
@@ -3,10 +3,12 @@ import requests
 
 GRAPH_VERSION = os.getenv("FB_GRAPH_VERSION", "v26.0")
 TOKEN = os.environ["FB_PAGE_ACCESS_TOKEN"]
-RAW_POST_ID = os.environ["FB_POST_ID"]\nPAGE_ID = RAW_POST_ID.split("_", 1)[0]\nPOST_ID = f"{PAGE_ID}_122126195721380231"
+RAW_POST_ID = os.environ["FB_POST_ID"]
+PAGE_ID = RAW_POST_ID.split("_", 1)[0]
+POST_ID = PAGE_ID + "_122126195721380231"
 
 r = requests.delete(
-    f"https://graph.facebook.com/{GRAPH_VERSION}/{POST_ID}",
+    "https://graph.facebook.com/" + GRAPH_VERSION + "/" + POST_ID,
     data={"access_token": TOKEN},
     timeout=30,
 )
@@ -15,5 +17,5 @@ print("BODY", r.text)
 r.raise_for_status()
 payload = r.json()
 if payload.get("success") is not True:
-    raise SystemExit(f"Delete did not return success=true: {payload}")
+    raise SystemExit("Delete did not return success=true: " + str(payload))
 print("DELETE_OK", POST_ID)
